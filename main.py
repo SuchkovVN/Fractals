4122370a84e8b4cc7f00a4999ede0444583540ad
Vladislav Suchkov <v.suchkov@yadro.com>
2024-01-01 16:01:25 +0300

    initial

---
+++ main.py
@@ -0,0 +1,75 @@
+import utils
+import multiprocessing
+import time
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
+coef = (-0.3 -0.2j)
+maxIter = 900
+radius = 2.5
+def iterate(zs):
+    res = np.zeros(zs.shape[0])
+    
+    i = 0
+    for zp in zs:
+        z = zp
+        k = 0
+        while abs(z) <= radius and k <= maxIter:
+            z = z**2 + coef
+            k += 1
+        res[i] = k / maxIter
+        i += 1
+    
+    return res
+
+def iterate_z(cs):
+    res = np.zeros(cs.shape[0])
+    
+    i = 0
+    for c in cs:
+        z = c
+        k = 0
+        while abs(z) <= 2 and k <= maxIter:
+            z = z**2 + c
+            k += 1
+        res[i] = k / maxIter
+        i += 1
+    
+    return res
+
+def main():
+    xmin = -1.5
+    xmax = 0.5
+    ymin = -1.1
+    ymax = 1.1
+    xwidth = xmax - xmin
+    yheight = ymax - ymin
+    im_width = 512
+    im_height = 512
+    net = (xmin, ymin, xwidth / im_width, yheight / im_height, im_width, im_height)
+        
+    start = time.clock_gettime(0)
+    # jset = utils.cmap_parallel(iterate, net, maxIter, coef, 2., 8)
+    mbrot = utils.mbrot_cmap_parallel(iterate_z, net, 8)
+    stop = time.clock_gettime(0) - start
+    print(f"Elapsed time: {stop}s")
+        
+    fig, ax = plt.subplots()
+    mbrot = np.transpose(mbrot)
+    # twilight_shifted for julia's set
+    plt.imsave(fname="mbrot2.png", arr=mbrot, cmap="flag_r", vmin=0, vmax=2.25)
+    plt.imshow(mbrot, cmap="flag_r", vmin=0, vmax=2.25)
+    
+    xtick_labels = np.linspace(xmin, xmax, int(xwidth * 2))
+    ax.set_xticks([(x-xmin) / xwidth * im_width for x in xtick_labels])
+    ax.set_xticklabels(['{:.1f}'.format(xtick) for xtick in xtick_labels])
+    ytick_labels = np.linspace(ymin, ymax, int(yheight * 2))
+    ax.set_yticks([(y-ymin) / yheight * im_height for y in ytick_labels])
+    ax.set_yticklabels(['{:.1f}'.format(ytick) for ytick in ytick_labels])
+    
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import multiprocessing
+import time
+
+# net = (x_start, y_start, x_step, y_step, x_dim, y_dim)
+
+def make_iter(c, maxIter, radius):
+    def iterate(z):
+        k = 0
+        while abs(z) <= radius and k <= maxIter:
+            z = z**2 + c
+            k += 1
+        return k / maxIter
+    return iterate
+    
+def julia_cmap_parallel(rhs, net, procs):
+    map = np.zeros((net[4], net[5]))
+    
+    x = net[0]
+    y = net[1]
+    i = 0
+    while i < map.shape[0] - procs:
+        zs = [None] * procs
+        
+        res = []
+        for d in range(0, procs):
+            zs[d] = np.zeros(map.shape[1], dtype=complex)
+            for j in range(0, zs[0].shape[0]):
+                zs[d][j] = complex(x + (i + d) * net[2], y + j * net[3])
+        
+        with multiprocessing.Pool(procs) as pool:
+            res = pool.map(rhs, zs)
+        
+        for r in res:
+            for j in range(0, map.shape[1]):
+                map[i, j] = r[j]
+            i += 1
+        
+    if map.shape[0] - i > 0:
+        dr = map.shape[0] - i
+        zs = [None] * dr
+        
+        res = []
+        for d in range(0, dr):
+            zs[d] = np.zeros(map.shape[1], dtype=complex)
+            for k in range(0, map.shape[1]):
+                zs[d][k] = complex(x + (i + d) * net[2], y + k * net[3])
+        
+        with multiprocessing.Pool(dr) as pool:
+            res = pool.map(rhs, zs)
+        
+        for r in res:
+            for j in range(0, map.shape[1]):
+                map[i, j] = r[j]
+            i += 1
+
+    return map
+
+
+def mbrot_cmap_parallel(rhs, net, procs):
+    map = np.zeros((net[4], net[5]))
+    
+    x = net[0]
+    y = net[1]
+    j = 0
+    while j < map.shape[1] - procs:
+        cs = [None] * procs
+        
+        res = []
+        for d in range(0, procs):
+            cs[d] = np.zeros(map.shape[0], dtype=complex)
+            for i in range(0, cs[0].shape[0]):
+                cs[d][i] = complex(x + i * net[2], y + (j + d) * net[3])
+        
+        with multiprocessing.Pool(procs) as pool:
+            res = pool.map(rhs, cs)
+        
+        for r in res:
+            for i in range(0, map.shape[0]):
+                map[i, j] = r[i]
+            j += 1
+        
+    if map.shape[0] - j > 0:
+        dr = map.shape[0] - j
+        cs = [None] * dr
+        
+        res = []
+        for d in range(0, dr):
+            cs[d] = np.zeros(map.shape[0], dtype=complex)
+            for k in range(0, map.shape[0]):
+                cs[d][k] = complex(x + k * net[2], y + (j + d) * net[3])
+        
+        with multiprocessing.Pool(dr) as pool:
+            res = pool.map(rhs, cs)
+        
+        for r in res:
+            for i in range(0, map.shape[0]):
+                map[i, j] = r[i]
+            j += 1
+
+    return map
